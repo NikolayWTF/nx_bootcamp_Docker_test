5d5c14fe8d6bfb7d367525f67d3c6396fdc9258b
NikolayWTF <kolan.gregorin01@gmail.com>
2021-05-16 20:38:03 +0300

    beta-1.0

---
+++ host/pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Николай Григорьев (kolan.gregorin01@gmail.com)'
+SITENAME = 'Мой NXBootcamp'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Всякие полезные ссылки
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Мои социальные сети
+SOCIAL = (('Мой ВКонтакте', 'https://vk.com/nikolayqwerty'),
+          ('Мой GitHub', 'https://github.com/NikolayWTF'),)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
